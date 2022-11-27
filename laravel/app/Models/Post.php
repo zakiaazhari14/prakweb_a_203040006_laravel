db66145b9cad8c09a7a42b899a6dae2254ef19e7
Zaki Auliya Azhari <zakiauliyaazhari@gmail.com>
2022-11-27 23:01:51 +0700

    Video #18 - Create Post Form

--- laravel/app/Models/Post.php
+++ laravel/app/Models/Post.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
-
+    use Sluggable;
     // protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
 
@@ -53,5 +54,13 @@ class Post extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
